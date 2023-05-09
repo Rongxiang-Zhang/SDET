f21eef3cfc21a81d85ffe73adcf97584def7db46
Sean Zhang <sean1zhang@gmail.com>
2023-05-08 23:08:59 -0500

    Add Maven dependencies and run simple test case in different browsers

--- RCVDemo/src/DemoAutomation.java
+++ RCVDemo/src/DemoAutomation.java
@@ -1,13 +1,17 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class DemoAutomation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     
-		System.setProperty("webdriver.chrome.driver", "D:\\Browserdrivers\\chromedriver.exe");
-		
-		ChromeDriver driver = new ChromeDriver();
+
+		System.setProperty("webdriver.edge.driver", "D:\\Browserdrivers\\msedgedriver.exe");
+
+//		ChromeDriver driver = new ChromeDriver();
+		EdgeDriver driver = new EdgeDriver();
+//		FirefoxDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
 	}
 
---
+++ SeleniumTraining/src/test/java/ui/DemoAutomation.java
@@ -0,0 +1,26 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "D:\\Browserdrivers\\chromedriver.exe");
+
+		ChromeDriver driver = new ChromeDriver();
+//		EdgeDriver driver = new EdgeDriver();
+//		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://www.ebay.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("mobile");
+		driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).click();
+		driver.close();
+		//driver.quit();
+	}
+
+}
