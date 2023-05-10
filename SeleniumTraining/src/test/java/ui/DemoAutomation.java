f49c57b695576cdfada800df9839d0c42a860dee
Sean Zhang <sean1zhang@gmail.com>
2023-05-09 23:03:34 -0500

    Write first testcase in selenium and understand the webdriver interface

--- SeleniumTraining/src/test/java/ui/DemoAutomation.java
+++ SeleniumTraining/src/test/java/ui/DemoAutomation.java
@@ -3,6 +3,9 @@ package ui;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class DemoAutomation {
@@ -10,11 +13,13 @@ public class DemoAutomation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver", "D:\\Browserdrivers\\chromedriver.exe");
-
+		//System.setProperty("webdriver.chrome.driver", "D:\\Browserdrivers\\chromedriver.exe");
+		
+		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 //		EdgeDriver driver = new EdgeDriver();
 //		FirefoxDriver driver = new FirefoxDriver();
+		
 		driver.get("http://www.ebay.com");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("mobile");
